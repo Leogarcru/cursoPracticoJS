28e40aa3d4e3b494dcc707ab9ff984d6be070441
LeoGarCru <leo.garcru@gmail.com>
2021-09-11 15:48:31 -0500

    Se cambia la operacio nde la pagina por cupones de descuento ya no se ingresa el numero de pocentaje del descuento, ahora se ingresa un texto que representa un cupon en un aray de objeto donde cada cupon tiene asignado un valor de descuento posible

--- descuento.js
+++ descuento.js
@@ -8,6 +8,12 @@
 //   precioConDescuento
 // })
 
+const cupones = [
+  {nombre: "SEIKA", valor: 15},
+  {nombre: "ESCON", valor: 25},
+  {nombre: "PLATZI", valor: 50}
+]
+
 function calcularPrecioConDescuento(precio, descuento){
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -15,13 +21,26 @@ function calcularPrecioConDescuento(precio, descuento){
 }
 
 function onClickButtonPriceDiscount(){
+  const resultP = document.getElementById("resultPrice");
+  resultP.innerText = "";
   const inputPrice = document.getElementById("inputPrice");
   const inputPriceValue = Number(inputPrice.value);
-  const inputDiscount = document.getElementById("inputDiscount");
-  const inputDiscountValue = Number(inputDiscount.value);
-  const precioConDescuento = calcularPrecioConDescuento(inputPriceValue, inputDiscountValue)
-  const resultP = document.getElementById("resultPrice");
-  resultP.innerText = "El precio con descuento es: $" + precioConDescuento
+  
+  const inputCupon = document.getElementById("inputDiscount");
+  const cuponName = cupones.find(cupon => cupon.nombre == inputCupon.value)
+  
+  if (cuponName == undefined){
+    // const resultP = document.getElementById("resultPrice");
+    resultP.innerText = "Cupon no valido, revisa tu ortografia";
+  } else {
+    const resultP = document.getElementById("resultPrice");
+    console.log(cuponName.valor)
+    // const inputDiscount = document.getElementById("inputDiscount");
+    // const inputDiscountValue = Number(inputDiscount.value);
+    const inputDiscountValue = Number(cuponName.valor);
+    const precioConDescuento = calcularPrecioConDescuento(inputPriceValue, inputDiscountValue)
+    resultP.innerText = "El precio con descuento es: $" + precioConDescuento
+  }
 }
 
 //.innerHTML vs .innerText
