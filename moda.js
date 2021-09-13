f89add707fc245ba1e377fade19e3892cf01a5ad
leogarcru <leo.garcru@gmail.com>
2021-09-13 18:10:05 -0500

    Se convierte el objeto en array Con ello se puede usar el metodo .sort() para ordenarlo de menor a mayor ocurrencia se toma el ultimo elemento de dicho array, pues tiene la maxima ocurrencia por lo tanto el elemento en moda

--- moda.js
+++ moda.js
@@ -1,5 +1,5 @@
 const lista1 = [
-  1,2,3,1,2,3,2,2,2,4
+  1,2,3,1,2,3,2,2,2,4,4,4,4,4,4,4,4,4
 ]
 
 const listaCount = {}
@@ -7,9 +7,20 @@ const listaCount = {}
 lista1.map(
   function(elemento){
     if (listaCount[elemento]){
-    listaCount[elemento] += 1
+      listaCount[elemento] += 1;
     } else {
-      listaCount[elemento] = 1
+      listaCount[elemento] = 1;
     }
   }
-)
+);
+
+const listaCountArray = Object.entries(listaCount);
+
+const listaCountArraySort = listaCountArray.sort(
+  function (a,b){
+    return a[1] - b[1];
+  }
+);
+
+console.log (listaCountArraySort[listaCountArraySort.length - 1])
+console.log (listaCountArraySort[listaCountArraySort.length - 1][0])
