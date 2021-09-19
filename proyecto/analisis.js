f777f4e2ea1ed93335d9f704a48bff92ce28cea8
LeoGarCru <leo.garcru@gmail.com>
2021-09-18 23:05:07 -0500

    se termina el curso se termina el curso en platzi pendiente de agregar CSS para mejorar el estilo

--- proyecto/analisis.js
+++ proyecto/analisis.js
@@ -1,29 +1,9 @@
-const salariosCol = colombia.map(
-  function(persona){
-    return persona.salary;
-  }
-)
-
-const salariosColSorted = salariosCol.sort(
-  function(a,b){
-    return a - b;
-  }
-)
-
+//Funciones Helpers
 function esPar(numero){
   return (numero % 2 === 0)
 }
 
-function calcularMedia(lista){
-  const suma = lista.reduce(
-    function(acumulado = 0, elemento){
-      return acumulado + elemento;
-    }
-  )
-  const promedio = suma / lista.length;
-  return promedio;
-}
-
+//Calculadora de mediana
 function medianaSalarios(lista){
   const mitad = parseInt(lista.length / 2);
   if (esPar(lista.length)){
@@ -36,4 +16,36 @@ function medianaSalarios(lista){
   }
 }
 
-console.log(medianaSalarios(salariosColSorted))
+const salariosCol = colombia.map(
+  function(persona){
+    return persona.salary;
+  }
+);
+
+const salariosColSorted = salariosCol.sort(
+  function(a,b){
+    return a - b;
+  }
+);
+
+
+function calcularMedia(lista){
+  const suma = lista.reduce(
+    function(acumulado = 0, elemento){
+      return acumulado + elemento;
+    }
+  )
+  const promedio = suma / lista.length;
+  return promedio;
+}
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted)
+
+//Mediana del Top 10%
+const spliceStart = Math.ceil(salariosColSorted.length * 0.9);
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(spliceStart, spliceCount);
+
+console.log(medianaSalarios(salariosColSorted));
+console.log(medianaSalarios(salariosColTop10));
